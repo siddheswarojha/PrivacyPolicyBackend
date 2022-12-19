1f971df7338b780ec0a57a9f41d8728ed93aaa19
siddheswarojha <siddheswarojha23@gmail.com>
2022-12-19 21:26:30 +0530

    code cleanup

--- src/main/java/com/example/privacypolicybackend/api/exception/PrivacyExceptions.java
+++ src/main/java/com/example/privacypolicybackend/api/exception/PrivacyExceptions.java
@@ -1,4 +1,8 @@
 package com.example.privacypolicybackend.api.exception;
 
-public class PrivacyExceptions {
+public class PrivacyExceptions  extends RuntimeException{
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
 }
